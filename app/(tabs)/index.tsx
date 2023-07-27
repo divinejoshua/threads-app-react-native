0b1721d4d80f758f092ee44082584c48f8d8ba5c
divinejoshua <divineuche399@gmail.com>
2023-07-27 16:00:16 +0100

    Added loading function

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useRef, useContext, useState } from "react";
 import {
   Platform,
   RefreshControl,
@@ -9,10 +10,24 @@ import {
 import Lottie from "lottie-react-native";
 import { ThreadContext } from "../../context/thread-context";
 import ThreadItem from "../../components/ThreadItem";
+import { Text, View } from "../../components/Themed";
 
 export default function TabOneScreen() {
-  const animationRef = React.useRef<Lottie>(null);
-  const threads = React.useContext(ThreadContext);
+  const animationRef = useRef<Lottie>(null);
+  const threads = useContext(ThreadContext);
+  const [isLoading, setisLoading] = useState(false) 
+
+
+  // Loading functionality 
+  const LoadData = () => {
+    // Set loading to true 
+    setisLoading(true)
+
+    // Set Loading back to false after a few seconds 
+    setTimeout(() => {
+      setisLoading(false);
+    }, 3000);
+  }
 
   React.useEffect(() => {
     animationRef.current?.play();
@@ -27,23 +42,36 @@ export default function TabOneScreen() {
         }}
         refreshControl={
           <RefreshControl
-            refreshing={false}
-            tintColor={"transparent"}
+            refreshing={true}
+            // tintColor={"transparent"}
             onRefresh={() => animationRef.current?.play()}
           />
         }
       >
-        <Lottie
+<View style={{
+   flex: 1, // Make sure the parent View takes the full available space
+   justifyContent: 'center', // Center vertically
+   alignItems: 'center', // Center horizontally
+   backgroundColor: 'yellow', // Optional: Add a background color to visualize the View's boundary
+}}>
+
+   {/* The lottie logo animation on refresh  */}
+   {/* <Lottie
           ref={animationRef}
           source={require("../../lottie-animations/threads.json")}
           style={{
             width: 90,
             height: 90,
-            alignSelf: "center",
+            // marginLeft: '18%',
           }}
           loop={false}
           onAnimationFinish={() => animationRef.current?.pause()}
-        />
+        /> */}
+        {/* <Text>Loading</Text> */}
+</View>
+       
+
+        {/* Loop through posts  */}
         {threads.map((thread) => (
           <ThreadItem key={thread.id} thread={thread} />
         ))}
